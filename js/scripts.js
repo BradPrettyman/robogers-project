5ac5ec3eb5f82c0fc7a7c6832ad363eb22b56749
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-20 10:39:30 -0700

    pass second test for replacing number 2 with boop

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ function numberInput(number) {
 
   let numberArray = [];
   let beep = 'Beep!';
+  let boop = 'Boop!';
 
   for (let i = 0; i <= input; i++) {
     numberString = i.toString();
 
     if (numberString.includes('1')) {
       numberArray.push(beep)
+    }
+    else if (numberString.includes('2')) {
+      numberArray.push(boop)
     }
   }
   return numberArray;
