1fb642ab3f23e0e35728061747b7181f41a74abe
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-20 09:54:52 -0700

    pass first test for replacing number 1 with beep

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function numberInput(number) {
+
+  let numberArray = [];
+  let beep = 'Beep!';
+
+  for (let i = 0; i <= input; i++) {
+    numberString = i.toString();
+
+    if (numberString.includes('1')) {
+      numberArray.push(beep)
+    }
+  }
+  return numberArray;
+}
