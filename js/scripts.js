92181f4ace70f4776ffc26bf2753973e40309da0
Brad Prettyman <brad.prettyman5@gmail.com>
2021-08-20 14:46:45 -0700

    WIP: getting tests to go through

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,24 @@ function numberInput(number) {
   let numberArray = [];
   let beep = 'Beep!';
   let boop = 'Boop!';
+  let neighbor = 'Wont you be my neighbor?';
+  let input = number
+
 
   for (let i = 0; i <= input; i++) {
     numberString = i.toString();
 
-    if (numberString.includes('1')) {
-      numberArray.push(beep)
+    if (numberString.includes('3')) {
+      numberArray.push(neighbor)
     }
     else if (numberString.includes('2')) {
       numberArray.push(boop)
+    }
+    else if (numberString.includes('1')) {
+      numberArray.push(beep)
+    }
+    else {
+      numberArray.push(numberString)
     }
   }
   return numberArray;
